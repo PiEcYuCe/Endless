e26efa8edc5035caee1796e9f9d5db0f625f07c6
nhiemIt <lytinhnhiem@gmail.com>
2024-05-23 06:18:42 +0700

    update product

--- src/main/java/com/java5/assignment/controllers/client/ProductController.java
+++ src/main/java/com/java5/assignment/controllers/client/ProductController.java
@@ -9,13 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.List;
 
 @Controller
