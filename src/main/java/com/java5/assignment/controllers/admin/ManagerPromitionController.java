cf084050cf8e5a6893fe3b13bc7565fa87ae4ad2
nmnhu2k4 <nhu9a318@gmail.com>
2024-05-26 18:47:34 +0700

    promotion

--- src/main/java/com/java5/assignment/controllers/admin/ManagerPromitionController.java
+++ src/main/java/com/java5/assignment/controllers/admin/ManagerPromitionController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
-public class ManagerPromition {
+public class ManagerPromitionController {
     @ModelAttribute("page")
     public Page page() {
         return Page.route.get(PageType.ADMIN_PROMOTION);
