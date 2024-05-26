2525e75e088f02579b062bad86e14be166b506d6
vtthaonguyen <nguyenvttpc05404@fpt.edu.vn>
2024-05-26 15:52:08 +0700

    vttnguyen update

--- src/main/java/com/java5/assignment/model/ProductVersion.java
+++ src/main/java/com/java5/assignment/model/ProductVersion.java
@@ -1,9 +1,14 @@
 package com.java5.assignment.model;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 
@@ -14,10 +19,16 @@ import java.math.BigDecimal;
 public class ProductVersion {
     private int productVersionID;
     private int productID;
+    @NotBlank(message = "Please enter version name")
     private String versionName;
+    @NotNull(message = "Please enter purchase price")
+    @DecimalMin("0")
     private BigDecimal purchasePrice;
+    @DecimalMin("0")
+    @NotNull(message = "Please enter version name")
     private BigDecimal salePrice;
-    private int quantity;
+    @NotNull(message = "Please enter quantity")
+    private Integer quantity;
     private boolean status;
-    private String image;
+    private MultipartFile image;
 }
