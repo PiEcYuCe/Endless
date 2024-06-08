619316d500c06e5b61a7ea3268eba667d6561aa9
nhiemIt <lytinhnhiem@gmail.com>
2024-06-08 15:16:22 +0700

    nhiemlt update entity rating

--- src/main/java/com/java5/assignment/jpa/ProductVersionRepository.java
+++ src/main/java/com/java5/assignment/jpa/ProductVersionRepository.java
@@ -1,7 +1,23 @@
 package com.java5.assignment.jpa;
 
+import com.java5.assignment.entities.Product;
 import com.java5.assignment.entities.ProductVersion;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductVersionRepository extends JpaRepository<ProductVersion, Long> {
+    @Query(value = "SELECT pv " +
+            "FROM ProductVersion pv " +
+            "JOIN pv.orderDetails od " +
+            "JOIN od.orderID o " +
+            "JOIN pv.productID p " +
+            "WHERE p.categoryID.id = :categoryId " +
+            "GROUP BY pv.id, pv.versionName, pv.purchasePrice, pv.price, pv.quantity, pv.status, pv.image, p " + // Thay p.id bằng p
+            "ORDER BY SUM(od.quantity) DESC")
+    List<ProductVersion> findByCategoryId(@Param("categoryId") Long categoryId, Pageable pageable);
 }
+
--- src/main/java/com/java5/assignment/model/share/login/ProfileModel.java
+++ src/main/java/com/java5/assignment/model/share/login/ProfileModel.java
@@ -1,4 +1,4 @@
-package com.java5.assignment.model.share;
+package com.java5.assignment.model.share.login;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
