d6884d710e1854c66bb3f1bb14037ec48008676d
vtthaonguyen <nguyenvttpc05404@fpt.edu.vn>
2024-06-11 18:54:09 +0700

    vttnguyen update

--- target/classes/src/main/resources/static/js/chooseStar.js
+++
@@ -1,9 +0,0 @@
-let chooseStar = function (number) {
-    for (let i = 0; i < number; i++) {
-        const starElement = document.getElementById('start' + i);
-        if (starElement) {
-            starElement.classList.remove('fa-regular', 'fa-star');
-            starElement.classList.add('fa-regular', 'fa-star');
-        }
-    }
-};
--- target/classes/src/main/resources/static/js/updateQuantity.js
+++
@@ -1,9 +0,0 @@
-function updateQuantity(itemId, quantityChange) {
-    var inputField = document.getElementById('quantityInput' + itemId);
-    var currentValue = parseInt(inputField.value);
-    var newValue = currentValue + quantityChange;
-    if (newValue < 1) {
-        newValue = 1;
-    }
-    inputField.value = newValue;
-}
